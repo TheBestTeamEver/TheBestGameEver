9583790f8c064e76eca25f39b72342def26e76b7
Pashok <pu.lysak@gmail.com>
2015-10-01 23:49:29 +0300

    Исправил бэк

---
+++ backend/src/main/java/main/Main.java
@@ -0,0 +1,77 @@
+package main;
+import admin.AdminPageServlet;
+import frontend.ExitServlet;
+import frontend.SignInServlet;
+import frontend.SignUpServlet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.Servlet;
+
+/**
+ * @author v.chibrikov
+ */
+
+public class Main {
+
+    public static final int PORT = 8080;
+
+    public static void main(String[] args) throws   InterruptedException {
+
+        if (args.length != 1) {
+            System.out.append("Use port as the first argument");
+            System.exit(1);
+        }
+        String portString = args[0];
+        int port;
+
+        try {
+            port = Integer.valueOf(portString);
+        }
+        catch (NumberFormatException ex) {
+
+            System.out.println("You have input not a number!!! Port 8080 will be used");
+            port = PORT;
+        }
+        System.out.println("Starting at port: " + port + '\n');
+
+        AccountService accountService = new AccountService();
+
+
+        Servlet signin = new SignInServlet(accountService);
+        Servlet signUp = new SignUpServlet(accountService);
+        Servlet exit   = new ExitServlet(accountService);
+        Servlet admin  = new AdminPageServlet(accountService);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(signin), "/api/v1/auth/signin");
+        context.addServlet(new ServletHolder(signUp), "/api/v1/auth/signup");
+        context.addServlet(new ServletHolder(exit),   "/api/v1/auth/logout");
+        context.addServlet(new ServletHolder(admin), AdminPageServlet.ADMIN_PAGE_URL);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(port);
+        server.setHandler(handlers);
+
+        try{
+            server.start();
+        }
+        catch (Exception ex){
+            //
+        }
+
+        server.join();
+
+
+    }
+}
