ba36e66ea024e973038f5f283a941de6f55a047d
alexss8 <leshenkagj@mail.ru>
2015-11-01 20:03:13 +0300

    View changed

--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -9,18 +9,27 @@ define([
     var View = Backbone.View.extend({
         el: '.page',
         template: tmpl,
+
         initialize: function () {
+            this.render()
             console.log("game view is initialized");
         },
+
         render: function () {
             this.$el.html(this.template);
-            return this;
+            console.log("game view rendered");
+            return this;//чтобы иметь возможность делать цепочные вызовы
         },
+
         show: function () {
-            this.$el.siblings(".button_back").show();
+            this.$el.show();
+            this.trigger('show', this);
+            console.log("game view showed");
         },
+
         hide: function () {
-            // TODO
+            this.$el.hide();
+            console.log("game view hided");
         }
 
     });
--- public_html/js/views/screboard.js
+++ public_html/js/views/screboard.js
@@ -11,12 +11,14 @@ define([
 ){
 
     var View = Backbone.View.extend({
-        el: '.main__container__page',
+        el: '.page',
         template: tmpl,
         collection: players,
         model: player,
+
         initialize: function () {
-            console.log("scoreboard view is initialized");
+            this.render()
+
             this.collection.push(new this.model({name: "Kirill", score: 99}));
             this.collection.push(new this.model({name: 'Natasha', score: 100}));
             this.collection.push(new this.model({name: 'Peter', score: 11}));
@@ -32,16 +34,25 @@ define([
             };
             this.collection.sort('score');
             console.log(this.collection.toJSON());
+
+            console.log("scoreboard view is initialized");
         },
+
         render: function () {
             this.$el.html(this.template(this.collection.toJSON()));
-            return this;
+            console.log("scoreboard view rendered");
+            return this;//чтобы иметь возможность делать цепочные вызовы
         },
+
         show: function () {
-            this.$el.siblings(".button_back").show();   
+            this.$el.show();
+            this.trigger('show', this);
+            console.log("scoreboard view showed");
         },
+
         hide: function () {
-            // TODO
+            this.$el.hide();
+            console.log("scoreboard view hided");
         }
 
     });
