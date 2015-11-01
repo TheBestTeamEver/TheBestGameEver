3734e7e8929279770b8a061aeb38d8b4dfb8703d
MaximZemskov <m.zemskov1@gmail.com>
2015-11-01 18:22:41 +0300

    new main View

--- public_html/js/views/main.js
+++ public_html/js/views/main.js
@@ -9,21 +9,27 @@ define([
     var View = Backbone.View.extend({
         el: '.page',
         template: tmpl,
-        initialize: function () {
 
+        initialize: function () {
+            this.render()
             console.log("main view is initialized");
         },
+
         render: function () {
             this.$el.html(this.template);
-            return this;
+            console.log("main view rendered");
+            return this;//чтобы иметь возможность делать цепочные вызовы
         },
+
         show: function () {
-            $(".menu__item_hide").on('click', function() {
-                //this.$el.find("menu__item_hide").show();
-            });
+            this.$el.show();
+            this.trigger('show', this);
+            console.log("main view showed");
         },
+
         hide: function () {
-            this.$el.find(".menu__item_hide").hide();
+            this.$el.hide();
+            console.log("main view hided");
         }
 
     });
