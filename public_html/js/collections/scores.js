74df38ea510870d1eb301a93b0a1b1659cad143f
alexss8 <leshenkagj@mail.ru>
2015-11-09 21:54:50 +0300

    scoreboard with collection changed

--- public_html/js/collections/scores.js
+++ public_html/js/collections/scores.js
@@ -7,8 +7,30 @@ define([
 ){
 
     var Players = Backbone.Collection.extend({
-    	model: player
+    	model: player,
+    	url:'/scores'
     });
 
-    return new Players();
+	var sortedByScore = Players.add([
+		new player({name:'Ostin', score: 543}),
+		new player({name:'Kianu', score: 3654}),
+		new player({name:'JJ', score: 375}),
+		new player({name:'CJ', score: 22}),
+		new player({name:'Artas', score: 1}),
+		new player({name:'Neo', score: 765}),
+		new player({name:'Lebovski', score: 978}),
+		new player({name:'Muhammed', score: 234}),
+		new player({name:'Tison', score: 897}),
+		new player({name:'Mandy', score: 6757})
+	]);
+
+	sortedByScore.sortBy(function (player) {
+
+		return player.get("score");
+
+	});
+
+
+
+    return new sortedByScore;
 });
--- public_html/js/views/scoreboard.js
+++ public_html/js/views/scoreboard.js
@@ -1,12 +1,10 @@
 define([
     'backbone',
     'tmpl/scoreboard',
-    'models/score',
     'collections/scores'
 ], function(
     Backbone,
     tmpl,
-    player,
     players
 ){
 
@@ -17,20 +15,6 @@ define([
         model: player,
         initialize: function () {
             console.log("scoreboard view is initialized");
-            this.collection.push(new this.model({name: "Kirill", score: 99}));
-            this.collection.push(new this.model({name: 'Natasha', score: 100}));
-            this.collection.push(new this.model({name: 'Peter', score: 11}));
-            this.collection.push(new this.model({name: 'Misha', score: 83}));
-            this.collection.push(new this.model({name: 'Kostya', score: 97}));
-            this.collection.push(new this.model({name: 'John', score: 42}));
-            this.collection.push(new this.model({name: 'Mike', score: 46}));
-            this.collection.push(new this.model({name: 'Miley', score: 79}));
-            this.collection.push(new this.model({name: 'Champion', score: 17}));
-            this.collection.push(new this.model({name: 'Anonimus', score: 94}));
-            this.collection.comparator = function(player) {
-                 return -player.get("score");
-            };
-            this.collection.sort('score');
             console.log(this.collection.toJSON());
         },
         render: function () {
