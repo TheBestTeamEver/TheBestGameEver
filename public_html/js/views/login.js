7c66cab80673e83727d5ad4e8eb4367ae3dfad1c
MaximZemskov <m.zemskov1@gmail.com>
2015-11-20 11:00:59 +0300

    sync fixed

--- public_html/js/views/login.js
+++ public_html/js/views/login.js
@@ -1,15 +1,19 @@
 define([
     'backbone',
     'tmpl/login',
-    'models/login'
+    'models/user'
 ], function(
     Backbone,
     tmpl,
-    login
+    user
 ){
 
     var View = Backbone.View.extend({
         template: tmpl,
+        user: user,
+        events: {
+            'click .login-form__input-submit__item' : 'login'
+        },
 
         initialize: function () {
             $('.page').append(this.el); 
@@ -28,6 +32,19 @@ define([
 
         hide: function () {
             this.$el.hide();
+        },
+
+        login: function() {
+            alert('LOGIN');
+
+            this.user.url = 'http://127.0.0.1:8080/api/v1/auth/signin/';
+            var login = $('input[name=name]').val();
+            var password = $('input[name=password]').val();
+
+            this.user.set({'name': login});
+            this.user.set({'password': password});
+
+            this.user.save();
         }
 
     });
